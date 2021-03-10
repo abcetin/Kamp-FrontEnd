f64fe347ac5919177e53ad409bea4332847659fb
TulparN <cetin.ab@outlook.com>
2021-03-11 00:07:48 +0300

    10 mart commit 1

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import {HttpClient} from '@angular/common/http';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 
 //axios,fetch bunlar reactnative de http client e karşılık gelenler
@@ -13,19 +12,20 @@ import { ProductResponseModel } from 'src/app/models/productResponseModel';
 export class ProductComponent implements OnInit {
 
   products:Product[] = [];
-  apiUrl = "https://localhost:44328/api/products/getall";
-  constructor(private httpClient:HttpClient) { }
+  dataLoaded = false;
+  
+  constructor(private productService : ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts(){
-    this.httpClient
-    .get<ProductResponseModel>(this.apiUrl)
-    .subscribe((response) =>{
-      this.products = response.data
+    this.productService.getProducts().subscribe(response =>{
+      this.products=response.data;
+      this.dataLoaded=true;
     });
+    
   }
 
 }
